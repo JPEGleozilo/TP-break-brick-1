3420dea3a0c434a795db294ca191d28e0c6ef2d0
TuNuevoUsuario <juliinsa436@gmail.com>
2025-08-12 18:56:57 -0300

    Initial Commit

---
+++ scenes/game.js
@@ -0,0 +1,18 @@
+export default class Game extends Phaser.Scene {
+  constructor() {
+    super('Game');
+  }
+
+  preload() {
+    this.load.image("whiteRectangle", "./public/assets/rectanguloWhite.png");
+    this.
+  }
+
+  create() {
+    // Initialize game objects here
+  }
+
+  update() {
+    // Game logic updates go here
+  }
+}
