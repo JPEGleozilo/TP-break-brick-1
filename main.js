c30338cfe5c793c911380f6a425d05cc5405b144
unknown <zilonkaleo@gmail.com>
2025-08-15 01:49:56 -0300

    Actividad v1

--- main.js
+++ main.js
@@ -1,34 +1,33 @@
-import HelloWorldScene from "./scenes/HelloWorldScene.js";
 import Game from "./scenes/game.js";
 
 // Create a new Phaser config object
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 600,
+  height: 400,
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
     min: {
-      width: 800,
-      height: 600,
+      width: 600,
+      height: 400,
     },
     max: {
-      width: 1600,
-      height: 1200,
+      width: 1200,
+      height: 800,
     },
   },
   physics: {
     default: "arcade",
     arcade: {
       gravity: { y: 200 },
-      debug: true,
+      debug: false,
     },
   },
   // List of scenes to load
   // Only the first scene will be shown
   // Remember to import the scene before adding it to the list
-  scene: [Game, HelloWorldScene],
+  scene: [Game],
 };
 
 // Create a new Phaser game instance
--- scenes/game.js
+++ scenes/game.js
@@ -5,14 +5,52 @@ export default class Game extends Phaser.Scene {
 
   preload() {
     this.load.image("whiteRectangle", "./public/assets/rectanguloWhite.png");
-    this.
+    this.load.image("blackRectangle", "./public/assets/rectangulo.png");
+    this.load.image("outlineRectangle", "./public/assets/rectanguloOutline.png");
+    this.load.image("ball", "./public/assets/ball.png");
   }
 
   create() {
-    // Initialize game objects here
+    
+    const background = this.add.image(300, 200, "whiteRectangle").setOrigin(0.5).setDisplaySize(600, 400).setDepth(-1);
+
+    this.player = this.physics.add.image(300, 350, "blackRectangle").setOrigin(0.5).setImmovable(true).setDepth(2)
+    .setDisplaySize(125, 20);
+    
+    this.player.body.setAllowGravity(false);
+    this.player.setCollideWorldBounds(true);
+
+    this.ball = this.physics.add.image(300, 275, "ball").setOrigin(0.5).setDepth(1).setBounce(1)
+    .setDisplaySize(25, 25);
+
+    this.ball.setCollideWorldBounds(true);
+
+    const brick = this.physics.add.image(Phaser.Math.Between(50, 550), Phaser.Math.Between(75, 250), "outlineRectangle").setOrigin(0.5).setImmovable(true).setDepth(0)
+    .setDisplaySize(100, 25);
+
+    brick.body.setAllowGravity(false);
+
+    this.physics.add.collider(this.ball, this.player, () => {
+        const angle = Phaser.Math.Angle.Between(this.ball.x, this.ball.y, this.player.x, this.player.y);
+        const speed = 325;
+        this.ball.setVelocity(-speed * Math.cos(angle), -speed);
+    });
+    this.physics.add.collider(this.ball, brick, () => {
+      brick.destroy();
+    })
+
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   update() {
-    // Game logic updates go here
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-350);
+    }
+    if (this.cursors.right.isDown) {
+      this.player.setVelocityX(350);
+    }
+    if (this.cursors.left.isUp && this.cursors.right.isUp) {
+      this.player.setVelocityX(0);
+    }
   }
 }
